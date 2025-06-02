a6e18bada2189a8d38520325ecac9eb485eb5a1c
Aaron George <101379811+Aaronk41@users.noreply.github.com>
2025-06-02 11:41:11 -0400

    Create owasptop10.js

---
+++ week3/owasptop10.js
@@ -0,0 +1,12 @@
+pie showData
+    title OWASP Top 10 2025 (Predicted)
+    "A01: Broken Access Control" : 21.2
+    "A02: Injection" : 19.8
+    "A03: Cryptographic Failures" : 15.4
+    "A04: Insecure Design" : 12.1
+    "A05: Vulnerable Components" : 9.7
+    "A06: SSRF" : 7.3
+    "A07: Identification Failures" : 6.5
+    "A08: Data Integrity" : 4.9
+    "A09: Security Misconfig" : 2.8
+    "A10: Logging Failures" : 1.3
