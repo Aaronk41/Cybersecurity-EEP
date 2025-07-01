1325eb8e02d5d8983510448f3d135b56592c836b
Aaron George <101379811+Aaronk41@users.noreply.github.com>
2025-07-01 13:08:21 -0400

    Create scanner.py

---
+++ week7/pyshield_antivirusupdate/scanner.py
@@ -0,0 +1,16 @@
+import os
+from utils import get_file_hash
+from signatures import malware_hashes
+from quarantine import quarantine_file
+
+def scan_folder(folder):
+    print(f"🔍 Scanning: {folder}")
+    for root, _, files in os.walk(folder):
+        for file in files:
+            full_path = os.path.join(root, file)
+            file_hash = get_file_hash(full_path)
+            if file_hash in malware_hashes:
+                print(f"⚠️ Threat Detected: {full_path}")
+                quarantine_file(full_path)
+            else:
+                print(f"✔️ Clean: {full_path}")
