e5688399b009274ac858897235c29158e38faef7
Aaron George <101379811+Aaronk41@users.noreply.github.com>
2025-07-29 13:45:12 -0400

    Create voice_helper.dart

---
+++ week11/pokedex_app/lib/voice_helper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class VoiceHelper {
+  static final FlutterTts _flutterTts = FlutterTts();
+
+  static Future<void> speak(String message) async {
+    await _flutterTts.setLanguage("en-US");
+    await _flutterTts.setPitch(1.0);
+    await _flutterTts.speak(message);
+  }
+}
