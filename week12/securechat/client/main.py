814d4e84ca6d45739d3ff91fdd39bc4bcf5526f4
Aaron George <101379811+Aaronk41@users.noreply.github.com>
2025-08-07 13:21:46 -0400

    Update main.py

--- week12/securechat/client/main.py
+++ week12/securechat/client/main.py
@@ -1,13 +1,30 @@
 import socket
+import threading
+
+def receive_messages(sock):
+    while True:
+        try:
+            msg = sock.recv(1024).decode()
+            if not msg:
+                break
+            print(f"\n{msg}")
+        except:
+            break
 
 def start_client(host='127.0.0.1', port=5555):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((host, port))
-    print("Connected to the server. Type your messages.")
+
+    print(client.recv(1024).decode(), end="")
+    username = input()
+    client.send(username.encode())
+
+    thread = threading.Thread(target=receive_messages, args=(client,), daemon=True)
+    thread.start()
 
     try:
         while True:
-            msg = input("You: ")
+            msg = input()
             client.send(msg.encode())
     except KeyboardInterrupt:
         print("\nDisconnected.")
