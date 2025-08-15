5ced40037719e3f4e01837b536be06e4d7bde157
Aaron George <101379811+Aaronk41@users.noreply.github.com>
2025-08-15 11:21:42 -0400

    Delete week11/pokedex_app directory

--- week11/pokedex_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'scanner_screen.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ScannerScreen(),
-  ));
-}
